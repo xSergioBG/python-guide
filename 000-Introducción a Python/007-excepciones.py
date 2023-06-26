19d643cf4e4b246958dc980eb42afbcad99c1f44
Sergio Bello <sergioalvin@hotmail.es>
2023-06-26 22:04:36 +0200

    Update 007-excepciones.py

--- 000-Introducción a Python/007-excepciones.py
+++ 000-Introducción a Python/007-excepciones.py
@@ -1,4 +1,4 @@
-# entrada_salida.py
+# 007-excepciones.py
 
 """
 Este programa muestra ejemplos de manejo de excepciones en Python.
@@ -14,22 +14,22 @@ def main():
         numero1 = int(input("Ingrese el primer número: "))
         numero2 = int(input("Ingrese el segundo número: "))
         resultado = numero1 / numero2
-        print(f"El resultado de la división es: {resultado}")
+        print("El resultado de la división es:", resultado)
     except ValueError:
         print("Error: Ingrese solo números enteros.")
     except ZeroDivisionError:
         print("Error: No se puede dividir entre cero.")
-    except Exception as error:
-        print(f"Error: {error}")
+    except Exception as e:
+        print("Error:", e)
 
     # Ejemplo de excepción personalizada
     try:
         nombre = input("Ingrese su nombre: ")
         if len(nombre) < 3:
             raise ValueError("El nombre debe tener al menos 3 caracteres.")
-        print(f"Hola, {nombre}!")
-    except ValueError as error:
-        print(f"Error: {error}")
+        print("Hola,", nombre + "!")
+    except ValueError as ve:
+        print("Error:", ve)
 
 if __name__ == "__main__":
     main()
